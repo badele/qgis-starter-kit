9b3311035ac4035c3683d15fa24a68745b8811e1
Bruno Adelé <bruno@adele.im>
2020-02-03 08:15:34 +0100

    Update metro project

--- README.md
+++ README.md
@@ -13,9 +13,13 @@ Geofabrik style
 <img width="50%" height="50%" src="./doc/qgis_screenshot.png"/>
 
 
+## Load or update datas
+```
+docker run --rm --name="qgis-desktop-master" -it -w /qgis-starter-kit -v /tmp:/tmp -v ${PWD}:/qgis-starter-kit  qgis/qgis:release-3_4 /qgis-starter-kit/init_datas.sh
+```
+
 ## Launch QGIS from docker container
 ```
 xhost +
-test ! -d .datas && docker run --rm --name="qgis-desktop-master" -it -w /qgis-starter-kit -v ${PWD}:/qgis-starter-kit  qgis/qgis:release-3_4 /qgis-starter-kit/init_datas.sh
 docker run --rm --name="qgis-desktop-master" -it -w /qgis-starter-kit -v /root:/root -v ${HOME}:/home/${USER} -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY qgis/qgis:release-3_4 qgis
 ```
--- init_datas.sh
+++ init_datas.sh
@@ -3,11 +3,32 @@
 REGIONS=("languedoc-roussillon" "midi-pyrenees" "provence-alpes-cote-d-azur" "rhone-alpes")
 TMP_DOWNLOAD=/tmp/qgis-download
 
+function get_overpass_data() {
+    LINE=$1
+
+    # [out:json]
+    # [timeout:120]
+    # ;
+    # relation
+    #   ["ref"="EV8"]
+    #   ["type"="route"];
+    # out;
+    # >;
+    # out skel;
+
+    URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0Arelation%5B%22ref%22%3D%22${LINE}%22%5D%5Btype%3Droute%5D%3B%20%0Aout%3B%3E%3Bout%20skel%3B%20%0A"
+    test ! -f ${TMP_DOWNLOAD}/veloroute-${LINE}.json && wget -O ${TMP_DOWNLOAD}/veloroute-${LINE}.json "${URL}"
+    test ! -f datas/veloroute/${LINE}.geojson && osmtogeojson ${TMP_DOWNLOAD}/veloroute-${LINE}.json > datas/veloroute/${LINE}.geojson
+}
+
 ##########################################################
 # Install requirements
 ##########################################################
 apt-get update
-apt-get install wget unzip
+apt-get install -y npm wget unzip
+
+# Install osmtogeojson
+npm install -g osmtogeojson
 
 ##########################################################
 # OSM datas (from geofrabrik)
@@ -17,23 +38,30 @@ apt-get install wget unzip
 mkdir -p ${TMP_DOWNLOAD}
 
 for REGION in $REGIONS; do
-    test ! -f .datas/${REGION}/README && wget -O ${TMP_DOWNLOAD}/${REGION}.shp.zip "http://download.geofabrik.de/europe/france/${REGION}-latest-free.shp.zip"
+    test ! -f datas/${REGION}/README && wget -O ${TMP_DOWNLOAD}/${REGION}.shp.zip "http://download.geofabrik.de/europe/france/${REGION}-latest-free.shp.zip"
 done
 
 # Unzip
 for REGION in $REGIONS; do
-    mkdir -p .datas/${REGION}
-    test ! -f .datas/${REGION}/README && unzip ${TMP_DOWNLOAD}/${REGION}.shp.zip -d .datas/${REGION}
+    mkdir -p datas/${REGION}
+    test ! -f datas/${REGION}/README && unzip ${TMP_DOWNLOAD}/${REGION}.shp.zip -d datas/${REGION}
 done
 
 ##########################################################
 # Data gouv
 ##########################################################
 
-test ! -f .datas/regions-20180101.shp && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
-test ! -f .datas/regions-20180101.shp && unzip  ${TMP_DOWNLOAD}/regions-20180101-shp.zip -d ".datas/"
+test ! -f datas/regions-20180101.shp && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
+test ! -f datas/regions-20180101.shp && unzip  ${TMP_DOWNLOAD}/regions-20180101-shp.zip -d "datas/"
 
 ##########################################################
 # Veloroute
 ##########################################################
-test ! -f .datas/gpx-arles-frontignan.gpx && wget -O .datas/gpx-arles-frontignan.gpx https://cycling.waymarkedtrails.org/api/details/relation/5429597/gpx
+
+mkdir -p datas/veloroute
+
+# get overpass veloroute
+get_overpass_data "EV8"
+get_overpass_data "EV17"
+get_overpass_data "V70"
+get_overpass_data "V66"
