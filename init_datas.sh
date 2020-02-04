edd852a5cb9f24b06bf727400e8a3800940f80d0
Bruno Adelé <bruno@adele.im>
2020-02-04 08:08:46 +0100

    Add Andora and Spain countries

--- init_datas.sh
+++ init_datas.sh
@@ -48,11 +48,20 @@ for REGION in $REGIONS; do
 done
 
 ##########################################################
-# Data gouv
+# Countries
 ##########################################################
 
-test ! -f datas/regions-20180101.shp && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
-test ! -f datas/regions-20180101.shp && unzip  ${TMP_DOWNLOAD}/regions-20180101-shp.zip -d "datas/"
+mkdir -p datas/countries/polygons
+
+# French regions
+test ! -f ${TMP_DOWNLOAD}/regions-20180101-shp.zip && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
+test ! -f datas/countries/polygons/regions-20180101.shp && unzip  ${TMP_DOWNLOAD}/regions-20180101-shp.zip -d datas/countries/polygons/
+
+# Andore and Spain countries
+URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0A%28%0A%20%20relation%289407%29%3B%0A%0A%20%20relation%28349044%29%3B%0A%20%20relation%28349045%29%3B%0A%20%20relation%28349033%29%3B%0A%20%20relation%28348981%29%3B%0A%20%20relation%28349048%29%3B%0A%20%20relation%28349013%29%3B%0A%20%20relation%28349041%29%3B%0A%20%20relation%28349052%29%3B%0A%20%20relation%28349053%29%3B%0A%20%20relation%28349043%29%3B%0A%20%20relation%28349050%29%3B%0A%20%20relation%28349036%29%3B%0A%20%20relation%28349055%29%3B%0A%20%20relation%28349047%29%3B%0A%20%20relation%28349027%29%3B%0A%20%20relation%28349042%29%3B%0A%20%20relation%28348991%29%3B%0A%20%20relation%285921806%29%3B%0A%20%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
+test ! -f ${TMP_DOWNLOAD}/andora_spain.json && wget -O ${TMP_DOWNLOAD}/andora_spain.json "${URL}"
+test ! -f datas/countries/polygons/andora_spain.geojson && osmtogeojson ${TMP_DOWNLOAD}/andora_spain.json > datas/countries/polygons/veloroute/andora_spain.geojson
+
 
 ##########################################################
 # Veloroute
