109e574930c5cdcef831883698cb6637c4267665
Bruno Adelé <bruno@adele.im>
2020-03-03 08:05:54 +0100

    Add geojson montpellier_admin_centre

--- init_datas.sh
+++ init_datas.sh
@@ -21,6 +21,23 @@ function get_overpass_ref_datas() {
     test ! -f datas/veloroute/${LINE}.geojson && osmtogeojson ${TMP_DOWNLOAD}/veloroute-${LINE}.json > datas/veloroute/${LINE}.geojson
 }
 
+function get_admin_centre() {
+    # [out:json]
+    # [timeout:120]
+    # ;
+    # (
+    # relation["name"="Montpellier"]["admin_level"=8]["boundary"="administrative"];
+    # );
+    # out body;
+    # >;
+    # out skel qt;
+
+    URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%0A%5Btimeout%3A120%5D%0A%3B%20%20%20%20%20%0A%28%0A%20%20relation%5B%22name%22%3D%22Montpellier%22%5D%5B%22admin_level%22%3D8%5D%5B%22boundary%22%3D%22administrative%22%5D%3B%0A%29%3B%0Aout%3B%0A%3E%3B%0Aout%20skel%3B"
+    test ! -f ${TMP_DOWNLOAD}/montpellier_admin_centre_boundary.json && wget -O ${TMP_DOWNLOAD}/montpellier_admin_centre_boundary.json "${URL}"
+    test ! -f datas/regions_provinces/languedoc-roussillon/montpellier_admin_centre_boundary.geojson && osmtogeojson ${TMP_DOWNLOAD}/montpellier_admin_centre_boundary.json > datas/regions_provinces/languedoc-roussillon/montpellier_admin_centre_boundary.geojson
+
+}
+
 ##########################################################
 # Install requirements
 ##########################################################
@@ -38,7 +55,7 @@ npm install -g osmtogeojson
 mkdir -p ${TMP_DOWNLOAD}
 
 for REGION in $REGIONS; do
-    test ! -f datas/${REGION}/README -o ! -f ${TMP_DOWNLOAD}/${REGION}.shp.zip && wget -O ${TMP_DOWNLOAD}/${REGION}.shp.zip "http://download.geofabrik.de/europe/france/${REGION}-latest-free.shp.zip"
+    test ! -f datas/regions_provinces/${REGION}/README -a ! -f ${TMP_DOWNLOAD}/${REGION}.shp.zip && wget -O ${TMP_DOWNLOAD}/${REGION}.shp.zip "http://download.geofabrik.de/europe/france/${REGION}-latest-free.shp.zip"
 done
 
 # Unzip
@@ -47,6 +64,9 @@ for REGION in $REGIONS; do
     test ! -f datas/regions_provinces/${REGION}/README && unzip ${TMP_DOWNLOAD}/${REGION}.shp.zip -d datas/regions_provinces/${REGION}
 done
 
+# Montpellier Boundary
+get_admin_centre
+
 ##########################################################
 # Countries
 ##########################################################
@@ -54,27 +74,27 @@ done
 mkdir -p datas/countries/polygons
 
 # French regions
-test ! -f ${TMP_DOWNLOAD}/regions-20180101-shp.zip -o ! -f datas/countries/polygons/regions-20180101.shp && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
+test ! -f ${TMP_DOWNLOAD}/regions-20180101-shp.zip -a ! -f datas/countries/polygons/regions-20180101.shp && wget -O ${TMP_DOWNLOAD}/regions-20180101-shp.zip https://www.data.gouv.fr/fr/datasets/r/aacf9338-8944-4513-a7b9-4cd7c2db2fa9
 test ! -f datas/countries/polygons/regions-20180101.shp && unzip  ${TMP_DOWNLOAD}/regions-20180101-shp.zip -d datas/countries/polygons/
 
 # Andorra and Spain countries
 URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0A%28%0A%20%20relation%289407%29%3B%0A%0A%20%20relation%28349044%29%3B%0A%20%20relation%28349045%29%3B%0A%20%20relation%28349033%29%3B%0A%20%20relation%28348981%29%3B%0A%20%20relation%28349048%29%3B%0A%20%20relation%28349013%29%3B%0A%20%20relation%28349041%29%3B%0A%20%20relation%28349052%29%3B%0A%20%20relation%28349053%29%3B%0A%20%20relation%28349043%29%3B%0A%20%20relation%28349050%29%3B%0A%20%20relation%28349036%29%3B%0A%20%20relation%28349055%29%3B%0A%20%20relation%28349047%29%3B%0A%20%20relation%28349027%29%3B%0A%20%20relation%28349042%29%3B%0A%20%20relation%28348991%29%3B%0A%20%20relation%285921806%29%3B%0A%20%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
-test ! -f ${TMP_DOWNLOAD}/andorra_spain.json -o ! -f datas/countries/polygons/andorra_spain.geojson && wget -O ${TMP_DOWNLOAD}/andorra_spain.json "${URL}"
+test ! -f ${TMP_DOWNLOAD}/andorra_spain.json -a ! -f datas/countries/polygons/andorra_spain.geojson && wget -O ${TMP_DOWNLOAD}/andorra_spain.json "${URL}"
 test ! -f datas/countries/polygons/andorra_spain.geojson && osmtogeojson ${TMP_DOWNLOAD}/andorra_spain.json > datas/countries/polygons/andorra_spain.geojson
 
 # Portugal
 URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0A%28%0Arelation%283920249%29%3B%0Arelation%285134927%29%3B%0Arelation%283738284%29%3B%0Arelation%283905929%29%3B%0Arelation%284104644%29%3B%0Arelation%284875183%29%3B%0Arelation%285130767%29%3B%0Arelation%281278415%29%3B%0Arelation%283905924%29%3B%0Arelation%285011694%29%3B%0Arelation%282897141%29%3B%0Arelation%285123448%29%3B%0Arelation%283459013%29%3B%0Arelation%285122644%29%3B%0Arelation%285143590%29%3B%0Arelation%283898131%29%3B%0Arelation%283967823%29%3B%0Arelation%283920285%29%3B%0Arelation%281629146%29%3B%0Arelation%281629145%29%3B%0Arelation%285378337%29%3B%0Arelation%281253665%29%3B%0Arelation%285400979%29%3B%0Arelation%285396677%29%3B%0Arelation%285404272%29%3B%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
-test ! -f ${TMP_DOWNLOAD}/portugal.json -o ! -f datas/countries/polygons/portugal.geojson && wget -O ${TMP_DOWNLOAD}/portugal.json "${URL}"
+test ! -f ${TMP_DOWNLOAD}/portugal.json -a ! -f datas/countries/polygons/portugal.geojson && wget -O ${TMP_DOWNLOAD}/portugal.json "${URL}"
 test ! -f datas/countries/polygons/portugal.geojson && osmtogeojson ${TMP_DOWNLOAD}/portugal.json > datas/countries/polygons/portugal.geojson
 
 # Italia
 URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0A%28%0Arelation%2853937%29%3B%0Arelation%2840137%29%3B%0Arelation%281783980%29%3B%0Arelation%2840218%29%3B%0Arelation%2842611%29%3B%0Arelation%28179296%29%3B%0Arelation%2840784%29%3B%0Arelation%28301482%29%3B%0Arelation%2844879%29%3B%0Arelation%2853060%29%3B%0Arelation%2841256%29%3B%0Arelation%2844874%29%3B%0Arelation%2840095%29%3B%0Arelation%287361997%29%3B%0Arelation%2839152%29%3B%0Arelation%2841977%29%3B%0Arelation%2845757%29%3B%0Arelation%2842004%29%3B%0Arelation%2845155%29%3B%0Arelation%2843648%29%3B%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
-test ! -f ${TMP_DOWNLOAD}/italia.json -o ! -f datas/countries/polygons/italia.geojson && wget -O ${TMP_DOWNLOAD}/italia.json "${URL}"
+test ! -f ${TMP_DOWNLOAD}/italia.json -a ! -f datas/countries/polygons/italia.geojson && wget -O ${TMP_DOWNLOAD}/italia.json "${URL}"
 test ! -f datas/countries/polygons/italia.geojson && osmtogeojson ${TMP_DOWNLOAD}/italia.json > datas/countries/polygons/italia.geojson
 
 # Schweiz
 URL="http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A120%5D%3B%0A%28%0Arelation%281690227%29%3B%0Arelation%281686344%29%3B%0Arelation%281685677%29%3B%0Arelation%281693971%29%3B%0Arelation%281688583%29%3B%0Arelation%281686448%29%3B%0Arelation%281686449%29%3B%0Arelation%281685673%29%3B%0Arelation%281686447%29%3B%0Arelation%281698314%29%3B%0Arelation%281701133%29%3B%0Arelation%281699639%29%3B%0Arelation%281686366%29%3B%0Arelation%281696112%29%3B%0Arelation%281686649%29%3B%0Arelation%281686666%29%3B%0Arelation%281687006%29%3B%0Arelation%281686631%29%3B%0Arelation%281686359%29%3B%0Arelation%281693811%29%3B%0Arelation%281687730%29%3B%0Arelation%281702421%29%3B%0Arelation%281686699%29%3B%0Arelation%281702420%29%3B%0Arelation%281702419%29%3B%0Arelation%281697347%29%3B%0A%29%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B"
-test ! -f ${TMP_DOWNLOAD}/schweiz.json -o ! -f datas/countries/polygons/schweiz.geojson && wget -O ${TMP_DOWNLOAD}/schweiz.json "${URL}"
+test ! -f ${TMP_DOWNLOAD}/schweiz.json -a ! -f datas/countries/polygons/schweiz.geojson && wget -O ${TMP_DOWNLOAD}/schweiz.json "${URL}"
 test ! -f datas/countries/polygons/schweiz.geojson && osmtogeojson ${TMP_DOWNLOAD}/schweiz.json > datas/countries/polygons/schweiz.geojson
 
 
